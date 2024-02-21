930c6c08f75d1693ea49cf97f787e451abcbca00
Pradumna <pradumna.bajoria@crestdatasys.com>
2024-02-21 07:05:47 +0530

    Added component for Add & multiply.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
+
+const Add = () => {
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  return (
+    <div className="operation">
+      <span>Add:</span>
+      <input
+        className="input"
+        type="number"
+        placeholder="Value 1"
+        value={value1}
+        onChange={(e) => {
+          setValue1(e.target.valueAsNumber);
+          setResult(value2 + e.target.valueAsNumber);
+        }}
+      />
+      <span>+</span>
+      <input
+        className="input"
+        type="number"
+        placeholder="Value 2"
+        value={value2}
+        onChange={(e) => {
+          setValue2(e.target.valueAsNumber);
+          setResult(value1 + e.target.valueAsNumber);
+        }}
+      />
+      <span>=</span>
+      <input
+        className="input"
+        type="number"
+        placeholder="Result"
+        value={result}
+        disabled
+      />
+    </div>
+  );
+};
+
+const Multiply = () => {
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  return (
+    <div className="operation">
+      <span>Multiply:</span>
+      <input
+        className="input"
+        type="number"
+        placeholder="Value 1"
+        value={value1}
+        onChange={(e) => {
+          setValue1(e.target.valueAsNumber);
+          setResult(value2 * e.target.valueAsNumber);
+        }}
+      />
+      <span>x</span>
+      <input
+        className="input"
+        type="number"
+        placeholder="Value 2"
+        value={value2}
+        onChange={(e) => {
+          setValue2(e.target.valueAsNumber);
+          setResult(value1 * e.target.valueAsNumber);
+        }}
+      />
+      <span>=</span>
+      <input
+        className="input"
+        type="number"
+        placeholder="Result"
+        value={result}
+        disabled
+      />
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Add />
+      <Multiply />
     </div>
   );
 }
